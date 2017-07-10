d019bcc8fb695ef9bd5fc238ed80fe6c4d57c5b8
Adrian Szymanski <adrian.szymanski@nokia.com>
2017-07-10 08:14:04 +0200

    clean up

--- svn/svn-diff-files.sh
+++ svn/svn-diff-files.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-svn diff -c $1 | grep Index | cut -d" " -f2 | tr "/" " " | tr "\\" " "
+if [ $# -eq 1 ]; then
+  svn diff -c $1 | grep Index | cut -d" " -f2 | tr "/" " " | tr "\\" " " | sed '/iml/d' | sed '/^\s*$/d'
+elif [ $# -eq 0 ]; then
+  svn diff | grep Index | cut -d" " -f2 | tr "/" " " | tr "\\" " " | sed '/iml/d' | sed '/^\s*$/d'
+else
+  echo "Invalid number of parameters"
+fi
