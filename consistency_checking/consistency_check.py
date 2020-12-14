1a7f0b10512bcc7118add40cf9968c9646cfc334
Losseh <aszymanski90@gmail.com>
2020-12-14 16:45:28 +0100

    Update consistency_check.py
    Fix erroneous and superfluous 2nd for

--- consistency_checking/consistency_check.py
+++ consistency_checking/consistency_check.py
@@ -3,14 +3,13 @@
 import hashlib, os, sys
 
 def hash(directory):
-  for files in os.listdir(directory):
-    for subfile in files:
-      full_dir_path = os.path.join(directory, subfile)
-      if os.path.isdir(full_dir_path):
-        dir_files = os.listdir(full_dir_path)
-        dir_files_names = ''.join(dir_files)
-        files_names_hash = hashlib.md5(dir_files_names.encode('utf-8')).hexdigest()
-        print('dir=', full_dir_path, ', #files=', len(dir_files), ', hash=', files_names_hash)
+  for subfile in os.listdir(directory):
+    full_dir_path = os.path.join(directory, subfile)
+    if os.path.isdir(full_dir_path):
+      dir_files = os.listdir(full_dir_path)
+      dir_files_names = ''.join(dir_files)
+      files_names_hash = hashlib.md5(dir_files_names.encode('utf-8')).hexdigest()
+      print('dir=', full_dir_path, ', #files=', len(dir_files), ', hash=', files_names_hash)
 
 if len(sys.argv) != 2:
   print('insufficient arguments!')
