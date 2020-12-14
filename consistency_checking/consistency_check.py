1304f563ad8f828197082597637738deab2fb769
Adrian Szymanski (ext.) <Adrian.Szymanski@partner.bmw.de>
2020-12-14 15:05:14 +0100

    python script

---
+++ consistency_checking/consistency_check.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import hashlib, os, sys
+
+def hash(directory):
+  for files in os.listdir(directory):
+    print(files)
+    for subfile in files:
+      print(subfile)
+      if not subfile.startswith('.'):
+        full_dir_path = os.path.join(directory, subfile)
+        dir_files = os.listdir(full_dir_path)
+        dir_files_names = ''.join(dir_files)
+        files_names_hash = hashlib.md5(dir_files_names.encode('utf-8')).hexdigest()
+        print('dir=', full_dir_path, ', #files=', len(dir_files), ', hash=', files_names_hash)
+
+if len(sys.argv) != 2:
+  print('insufficient arguments!')
+  sys.exit(-1)
+
+hash(sys.argv[1])
