c0096f637b72a275bb06e7711bd31f49e84c682f
Adrian Szymanski (ext.) <Adrian.Szymanski@partner.bmw.de>
2020-12-15 10:41:23 +0100

    beautify consistency_check script

--- consistency_checking/consistency_check.py
+++ consistency_checking/consistency_check.py
@@ -1,18 +1,22 @@
 #!/bin/python
+# example usage = python consistency_check.py toshiba_device /home/outer/foteczki
 
 import hashlib, os, sys
 
-def hash(directory):
+def hash(name, directory):
+  print('report name={}, dir={}'.format(name, os.path.abspath(directory)))
   for subdir in os.listdir(directory):
     full_dir_path = os.path.join(directory, subdir)
     if os.path.isdir(full_dir_path):
       dir_files = os.listdir(full_dir_path)
       dir_files_names = ''.join(dir_files)
-      files_names_hash = hashlib.md5(dir_files_names.encode('utf-8')).hexdigest()
-      print('dir=', full_dir_path, ', #files=', len(dir_files), ', hash=', files_names_hash)
+      files_names_hash = hashlib.sha256(dir_files_names.encode('utf-8')).hexdigest()
+      print('dir={}, #files={}, hash={}'.format(subdir, len(dir_files), files_names_hash))
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
   print('insufficient arguments!')
   sys.exit(-1)
 
-hash(sys.argv[1])
+name = sys.argv[1]
+directory = sys.argv[2]
+hash(name, directory)
